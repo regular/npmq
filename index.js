68543ff64d5f7278cdc1d397a3cd73fef8786ed2
Jan Bölsche <regular.gonzales@googlemail.com>
2017-06-24 20:12:52 +0200

    Do query and import in one process. Findw high quality modules

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 var pull = require('pull-stream')
+const spawn = require('pull-spawn-process')
 var Flume = require('flumedb')
 //var FlumeQuery = require('flumeview-query')
 var Index = require('flumeview-level')
+var Reduce = require('flumeview-reduce')
 const sort = require('pull-sort')
 const many = require('pull-many')
+
+const transformRecords = require('./transform')
 
 var OffsetLog = require('flumelog-offset')
 var codec = require('flumecodec')
 
-function getName(e) {
-  if (typeof e.author === 'string') return e.author
-  return (e.author || {}).name || ""
-}
-
-function getUser(e) {
-  return ( (e._npmUser && e._npmUser.name) || (e.maintainers && e.maintainers[0] && e.maintainers[0].name)) || ""
-}
-
 const db = Flume(OffsetLog("flume-data/flume-npm.db", codec.json))
+  .use('numRecords', Reduce(1, (acc, item) => (acc || 0) + 1 ))
   .use('deps', Index(8, function (e) {
     // pull-stream:npm-ssb@1.1.0:~2.4.x
     let deps = e.dependencies||{};
@@ -45,6 +41,15 @@ const db = Flume(OffsetLog("flume-data/flume-npm.db", codec.json))
     let [name,version] = e._id.split('@')
     return [getUser(e)+":"+name]
   }))
+
+function getName(e) {
+  if (typeof e.author === 'string') return e.author
+  return (e.author || {}).name || ""
+}
+
+function getUser(e) {
+  return ( (e._npmUser && e._npmUser.name) || (e.maintainers && e.maintainers[0] && e.maintainers[0].name)) || ""
+}
 
 function byPackageName(name) {
   return db.id.read({
@@ -133,16 +138,46 @@ function whatDoTheyUse() {
   )
 }
 
-pull(
-  //publishedByUser('regular'),
-  many([
-    publishedByUser('raynos'),
-    publishedByUser('rvagg'),
-    publishedByUser('substack'),
-    authoredBy('dominictarr')
-  ]),
-  whatDoTheyUse(),
-  pull.take(40),
-  //details(),
-  logAndCount()
-)
+function appendLogStream(inputfile) {
+  let i = 0
+  setInterval( ()=>{
+    db.numRecords.get( (err, records) => {
+      process.stderr.write(`\rSyncing ... (${records} records in log. Hit Ctrl-C to exit.) ${'⠁⠃⠇⠃'[i = (i+1) % 4]}`)
+    })
+  }, 1000)
+
+  db.numRecords.get( (err, records) => {
+    console.log(`Replicating records since #${records}`)
+    pull(
+      spawn('tail', `-q -F +${records} ${inputfile}`.split(' ')),
+      //pull.map( (b)=>b.toString() ),
+      //pull.through(console.log),
+      transformRecords(),
+      pull.asyncMap(db.append),
+      pull.onEnd( (err)=>{
+        if (err) console.error(err.message)
+        console.log('sync aborted.')
+      })
+    )
+  })
+}
+
+appendLogStream(__dirname + '/hypercore/data')
+
+function goodModules() {
+  pull(
+    //publishedByUser('regular'),
+    many([
+      publishedByUser('raynos'),
+      publishedByUser('rvagg'),
+      publishedByUser('substack'),
+      authoredBy('dominictarr')
+    ]),
+    whatDoTheyUse(),
+    pull.take(30),
+    //details(),
+    logAndCount()
+  )
+}
+
+goodModules()
---
+++ transform.js
@@ -0,0 +1,34 @@
+var pull = require('pull-stream')
+var ndjson = require('pull-ndjson')
+
+var optionalProps = [
+    'gitHead',
+    '_npmUser',
+    'license',
+    '_npmVersion',
+    'dependencies',
+    'devDependencies',
+    'maintainers',
+    'contributors']
+
+module.exports = function() {
+  return pull(
+    ndjson.parse(),
+    pull.map( (m)=>{
+      var r = {
+        _id: m._id,
+        author: m.author || {name: ""},
+      }
+      if (m.dist && m.dist.tarball) r.tarball = m.dist.tarball
+      if (m.dist && m.dist.integrity) r.integrity = m.dist.integrity
+      if (m.repository && m.repository.url) r.repo = m.repository.url
+      var sha = m._shasum || (m.dist || {}).shasum
+      if (sha) r.sha = sha
+      optionalProps.forEach( (n)=>{
+        if (m[n]) r[n] = m[n]
+      })
+      return r
+    })
+  )
+}
+
